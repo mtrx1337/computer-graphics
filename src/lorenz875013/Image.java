dfd8a55c3125e12427b42917f5ffdfcb73ff2af1
Leonard Lorenz <contact@metr1xx.de>
2018-10-09 13:06:14 +0200

    finished task 1.4 and task 1.5

--- src/lorenz875013/Image.java
+++ src/lorenz875013/Image.java
@@ -1,18 +1,29 @@
-package tramberend7215;
+package lorenz875013;
 
+import cgtools.ImageWriter;
 import cgtools.Vec3;
+
 import java.io.IOException;
 
 public class Image {
+    int width;
+    int height;
+    double[] image;
+
     public Image(int width, int height) {
-        System.out.println("Image not yet implemented.");
+        this.image = new double[3 * (width * height)];
+        this.width = width;
+        this.height = height;
     }
 
     public void setPixel(int x, int y, Vec3 color) {
-        System.out.println("Image not yet implemented.");
+        int pixelAdress = 3 * (y * width + x);
+        image[pixelAdress] = color.x;
+        image[pixelAdress + 1] = color.y;
+        image[pixelAdress + 2] = color.z;
     }
 
     public void write(String filename) throws IOException {
-        System.out.println("Image not yet implemented.");
+        ImageWriter.write(filename, image, width, height);
     }
 }
--- src/lorenz875013/a01/Main.java
+++ src/lorenz875013/a01/Main.java
@@ -1,16 +1,20 @@
-package tramberend7215.a01;
+package lorenz875013.a01;
 
 import cgtools.Vec3;
 import static cgtools.Vec3.*;
 import java.io.IOException;
 import java.util.function.*;
-import tramberend7215.Image;
+import lorenz875013.Image;
 
 public class Main {
 
     public static void main(String[] args) {
-        final int width = 160;
-        final int height = 90;
+        //final int width = 160;
+        //final int height = 90;
+        final int width = 3440;
+        final int height = 1440;
+
+        /** static color **/
 
         Image image = new Image(width, height);
 
@@ -32,7 +36,73 @@ public class Main {
                 image.setPixel(x, y, allGray.pixelColor(x, y));
             }
         }
+
         write(image, "doc/a01.png");
+
+        /** square image **/
+
+        Image imageSquare = new Image(width, height);
+
+        class ColoredSquare{
+            Vec3 color;
+
+            ColoredSquare(Vec3 color) { this.color = color; }
+
+            Vec3 pixelColor(double x, double y){ return color;}
+        }
+
+        ConstantColor squareColor = new ConstantColor(Vec3.red);
+        int imageMiddleX = width / 2;
+        int imageMiddleY = height / 2;
+        double squareSizeMultiplier = 0.8;
+        int squareRadius;
+        if(height > width){
+            squareRadius = (int) (width * squareSizeMultiplier / 2);
+        } else {
+            squareRadius = (int) (height * squareSizeMultiplier / 2);
+        }
+
+        for (int x = 0; x != width; x++) {
+            for (int y = 0; y != height; y++) {
+                /** define left and right space around the square **/
+                if(x > imageMiddleX - squareRadius && x < imageMiddleX + squareRadius) {
+                    /** define top and bottom space around the square **/
+                    if(y > imageMiddleY - squareRadius && y < imageMiddleY + squareRadius) {
+                        image.setPixel(x, y, squareColor.pixelColor(x, y));
+                    }
+                }
+            }
+        }
+
+        write(image, "doc/a01-square");
+
+        /** checkerboard **/
+
+        Image imageCheckerBoard = new Image(width, height);
+
+        // size in pixels on how wide one box of the checker board is
+        // a checker board has 8 squares
+        int checkerBoxSize = (squareRadius * 2) / 8;
+        boolean primaryColor = true;
+        for (int x = 0; x != width; x++) {
+            for (int y = 0; y != height; y++) {
+                /** define left and right space around the square **/
+                if(x > imageMiddleX - squareRadius && x < imageMiddleX + squareRadius) {
+                    /** define top and bottom space around the square **/
+                    if(y > imageMiddleY - squareRadius && y < imageMiddleY + squareRadius) {
+                        /** define the checkerboard **/
+                    }
+                } else {
+                    if (primaryColor){
+
+                    } else {
+
+                    }
+                }
+            }
+        }
+
+        write(imageCheckerBoard, "doc/a01-checkered-background.png");
     }
 
     static void write(Image image, String filename) {
