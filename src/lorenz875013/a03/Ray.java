d211d032b525c89bc5d7497c597ee3f5e8ef385f
Leonard Lorenz <contact@metr1xx.de>
2018-10-23 11:21:02 +0200

    added new classes of task 3.1 and 3.2, implemented shootRay()

--- src/lorenz875013/a03/Ray.java
+++ src/lorenz875013/a03/Ray.java
@@ -9,10 +9,13 @@ public class Ray {
     double max;
 
     public Ray(Vec3 origin, Vec3 normDir, double min, double max){
-        
+        this.origin = origin;
+        this.normDir = normDir;
+        this.min = min;
+        this.max = max;
     }
 
-    Vec3 pointAt(double t){
-
+    public Vec3 pointAt(double t){
+        return origin.add(Vec3.multiply(t, normDir));
     }
 }
